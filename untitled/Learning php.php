1e73c559b72ef9373dece32db4de639ba58662d7
unknown <lbv2@njit.edu>
2019-03-05 15:41:51 -0500

    first commit-again

---
+++ untitled/Learning php.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lloum
+ * Date: 2/21/2019
+ * Time: 11:05 AM
+ */
+public function testArray()
+{
+    $array=
+           [
+               'one'=>'blue'
+               'two'=>'green'
+               'three'=>'red'
+
+
+           ]
+print_r($array)
+}
